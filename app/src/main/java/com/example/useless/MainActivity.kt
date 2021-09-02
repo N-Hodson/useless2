e87002ad8d5e5a9e593e1595f3273a529989f891
per2 <per2>
2021-09-02 09:21:48 -0700

    working

--- app/src/main/java/com/example/useless/MainActivity.kt
+++ app/src/main/java/com/example/useless/MainActivity.kt
@@ -4,10 +4,13 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.Switch
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.Group
 
 //switch useless
 // self destruct button
@@ -20,12 +23,16 @@ class MainActivity : AppCompatActivity() {
     lateinit var useless: Switch
     lateinit var destruct: Button
     lateinit var busy: Button
-    lateinit var layout : ConstraintLayout
+    lateinit var layout: ConstraintLayout
+    lateinit var UIgroup: Group
+    lateinit var Loadgroup: Group
+    lateinit var Bar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         wireWidgets()
+        Loadgroup.visibility = View.INVISIBLE
 
         useless.setOnCheckedChangeListener { buttonView, ischecked ->
             if (!ischecked) {
@@ -37,26 +44,23 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
-    }
 
-    private fun sefDestruct() {
         destruct.setOnClickListener {
-            var timesChanged = 0
-            if (timesChanged > 10) {
-                timesChanged = 0
-                finish()
-            } else {
-                timesChanged++
-                startDestruct1()
-                layout.setBackgroundColor(Color.argb(255, 255, 255, 255))
-            }
+            startDestruct1()
+        }
+        busy.setOnClickListener {
+            lookBusy()
         }
     }
+
     private fun wireWidgets() {
         useless = findViewById(R.id.switch_main_useless)
         destruct = findViewById(R.id.button_main_destruct)
         busy = findViewById(R.id.button_main_busy)
         layout = findViewById(R.id.layout)
+        UIgroup = findViewById(R.id.Group_Main_UI)
+        Loadgroup = findViewById(R.id.Group_Main_loader)
+        Bar = findViewById(R.id.progressBar_Main_Load)
     }
 
     private fun startSwitchTimer() {
@@ -72,16 +76,42 @@ class MainActivity : AppCompatActivity() {
         uselessTimer.start()
 
     }
-    private fun startDestruct1() {
-            val uselessTimer = object : CountDownTimer(3000, 1000) {
-                override fun onTick(millisUntilFinished: Long) {
-                }
 
-                override fun onFinish() {
-                    layout.setBackgroundColor(Color.argb(255, 255, 0, 0))
-                    sefDestruct()
-                }
+    private fun startDestruct1() {
+
+        val uselessTimer = object : CountDownTimer(10000, 100) {
+            override fun onTick(millisUntilFinished: Long) {
+                val red = (Math.random() * 255 + 1).toInt()
+                val green = (Math.random() * 255 + 1).toInt()
+                val blue = (Math.random() * 255 + 1).toInt()
+                layout.setBackgroundColor(Color.argb(255, red, green, blue))
+                destruct.setText("seconds remaining: " + millisUntilFinished / 1000)
             }
-            uselessTimer.start()
+
+            override fun onFinish() {
+                layout.setBackgroundColor(Color.argb(255, 255, 255, 255))
+                finish()
+            }
         }
+        uselessTimer.start()
     }
+
+    private fun lookBusy() {
+        UIgroup.visibility = View.INVISIBLE
+        Loadgroup.visibility = View.VISIBLE
+        var x = 0
+        val uselessTimer = object : CountDownTimer(30000, 300) {
+            override fun onTick(millisUntilFinished: Long) {
+
+                Bar.progress = x
+                x++
+            }
+
+            override fun onFinish() {
+                Loadgroup.visibility = View.INVISIBLE
+                UIgroup.visibility = View.VISIBLE
+            }
+        }
+        uselessTimer.start()
+    }
+}
